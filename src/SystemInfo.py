81d89e8ce8904e60befddadb71f1130330424cd4
Lakshantha Dissanayake <lakshanthad@yahoo.com>
2021-05-25 09:22:25 +0530

    Fix: Link data to System Information block

--- src/SystemInfo.py
+++ src/SystemInfo.py
@@ -1,17 +1,24 @@
 import sys
 import os
+import subprocess
 from PySide2.QtCore import *
+
+CM4 = subprocess.getoutput("cat /proc/cpuinfo | grep 'Revision' | awk '{print $1,$2,$3}'")
+RPiOS = subprocess.getoutput('lsb_release -ds')
+Linux = subprocess.getoutput('uname -r')
+Network = str(subprocess.check_output(['hostname', '-I'])).split(' ')[0].replace("b'", "")
+
 class Systeminfo(QThread):
     SystemSignal = Signal(str,str,str,str,str,str)
     def __init__(self):
         super().__init__()
     
     def run(self):
-        Compute = "CM46666"
-        Retermial = "RT666"
-        Version = "Raspberry Pi OS:Jan.12th 2012"
-        Kernel = "6.9"
-        Ethernet = "192.168.4.253"
-        Wifi = "okey"
+        Compute = CM4
+        Retermial = "V1.2"
+        Version = RPiOS
+        Kernel = Linux
+        Ethernet = Network
+        Wifi = Network
         self.sleep(1)
         self.SystemSignal.emit(Compute,Retermial,Version,Kernel,Ethernet,Wifi)
